99137127dc491258d1fb02749017b264d9fbc795
Matt Scott <mattscott251@gmail.com>
2023-08-24 15:39:15 -0500

    completed quick sort

--- quicksort.js
+++ quicksort.js
@@ -1,18 +1,36 @@
 function quicksort(arr) {
 
   // Check if the input is length 1 or less
+  if (arr.length <= 1) {
     // If so, it's already sorted: return
+    return arr;
+  }
 
   // Pick the first value as the pivot
+  let pivot = arr[0];
+  let leftHalf = [];
+  let rightHalf = [];
 
   // Orient the pivot so that...
-      // every number smaller than the pivot is to the left
-      // every number larger (or equal) than the pivot is to the right
+  for (let i = 1; i < arr.length; i++) {
+    // every number smaller than the pivot is to the left
+    if (arr[i] < pivot) {
+      leftHalf.push(arr[i]);
+    }
+    // every number larger (or equal) than the pivot is to the right
+    else {
+      rightHalf.push(arr[i]);
+    }
+  }
 
   // Recursively sort the left
+  let lhRtn = quicksort(leftHalf);
   // Recursively sort the right
+  let rhRtn = quicksort(rightHalf);
 
   // Return the left, pivot and right in sorted order
+  lhRtn.push(pivot); // pivot was assigned a single val from an array and is therefore a number. So use push method
+  return (lhRtn.concat(rhRtn));
 
 }
 
---
+++ test-code.js
@@ -0,0 +1,8 @@
+let a = [1,2,3];
+let b = 5;
+let c = [7,8,9];
+
+a.push(b);
+let x = a.concat(c);
+
+console.log(x);
